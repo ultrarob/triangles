0b3b2d58a456f57e538ce9b10adcdfb1d44c169a
ultrarob <rob@ultrarob.com>
2018-10-31 20:50:54 -0600

    Added documentation for TriangleService and a couple other tweaks.

--- Triangles/Services/TriangleService.cs
+++ Triangles/Services/TriangleService.cs
@@ -4,16 +4,33 @@ using Triangles.Exceptions;
 
 namespace Triangles.Services
 {
+   /// <summary>
+   /// Service to provide additional info about a triangle. The triangles are right triangles 
+   /// in a grid with rows A-F and columns 1-12. The triangles alternate as lower left and upper
+   /// right halves of a square.
+   /// </summary>
    public class TriangleService
    {
       // Length of non-hypotenuse sides
-      const int NON_HYPOTENUSE_LENGTH = 10;
+      private const int NON_HYPOTENUSE_LENGTH = 10;
       // Constants for validating row and column
-      const char MIN_ROW = 'a';
-      const char MAX_ROW = 'f';
-      const int MIN_COLUMN = 1;
-      const int MAX_COLUMN = 12;
+      private const char MIN_ROW = 'a';
+      private const char MAX_ROW = 'f';
+      private const int MIN_COLUMN = 1;
+      private const int MAX_COLUMN = 12;
 
+
+      /// <summary>
+      /// Given a row and column for a triangle, the coordinates of the vertices are calculated.
+      /// </summary>
+      /// <param name="row">The row for the triangle</param>
+      /// <param name="column">The column for the triangle</param>
+      /// <returns>
+      /// A <code>Triangle</code> object for the specified row and column.
+      /// </returns>
+      /// <exception cref="Triangles.Exceptions.TriangleException">
+      /// Thrown when an invalid row or column is supplied.
+      /// </exception>
       public Triangle GetTriangle(char row, int column)
       {
          // First check that the row and column are valid
@@ -62,17 +79,41 @@ namespace Triangles.Services
          return triangle;
       }
 
+      /// <summary>
+      /// Given the vertices of a triangle, the row and column are calculated.
+      /// </summary>
+      /// <param name="vertex1">Vertex 1 for the triangle</param>
+      /// <param name="vertex2">Vertex 2 for the triangle</param>
+      /// <param name="vertex3">Vertex 3 for the triangle</param>
+      /// <returns>
+      /// A <code>Triangle</code> object for the specified vertices.
+      /// </returns>
+      /// <exception cref="Triangles.Exceptions.TriangleException">
+      /// Thrown when an invalid vertex is supplied.
+      /// </exception>
       public Triangle GetTriangle(Point vertex1, Point vertex2, Point vertex3)
       {
-         // First need to determine if the triangle is the lower left or upper right
-         // triangle of a square and but the vertices into a known order
+         // First do some validation on the vertices
+
+         // Determine if the triangle is the lower left or upper right
+         // triangle of a square and put the vertices into a known order.
          bool lowerLeftTriangle = OrderVertices(ref vertex1, ref vertex2, ref vertex3);
 
          // TODO:
          return null;
       }
 
-      // TODO: comment on vertices order
+      /// <summary>
+      /// This method puts the vertices of a triangle in a known order and determines if the triangle is the lower
+      /// left triangle of a square or the upper right triangle. For the lower left triangle, the vertices will be
+      /// in the order of lower left vertex, upper left vertex and then lower right vertex.  For the upper right 
+      /// triangle, the vertices will be in the order of upper right vertex, upper left vertex and then the lower right
+      /// vertex.
+      /// </summary>
+      /// <returns>
+      /// <code>true</code> if the vertices are for the lower left triangle and <code>false</code>
+      /// if they are for the upper right triangle.
+      /// </returns>
       private bool OrderVertices(ref Point vertex1, ref Point vertex2, ref Point vertex3)
       {
          // We need to determine if the triangle is the lower left or upper right
