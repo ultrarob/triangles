1384b5e2b4e7dea20d2f6622be982601370a0fbd
ultrarob <rob@ultrarob.com>
2018-11-01 21:49:51 -0600

    Connected TriangleService GetTriangle() to api

---
+++ Triangles/Controllers/TriangleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+using Triangles.Models;
+using Triangles.Services;
+
+namespace Triangles.Controllers
+{
+   public class TriangleController : ApiController
+   {
+      private TriangleService triangleService;
+
+      TriangleController()
+      {
+         triangleService = new TriangleService();
+      }
+
+      // GET api/triangle/A/1
+      [Route("api/triangle/{row}/{column}")]
+      public Triangle Get(char row, int column)
+      {
+         return triangleService.GetTriangle(row, column);
+      }
+
+      // POST api/values
+      public void Post([FromBody]string value)
+      {
+      }
+
+   }
+}
